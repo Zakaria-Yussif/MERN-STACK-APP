4175f7c6fe34a3db331ebf23d5465cbfe2cf6f13
Zakaria-Yussif <zackyaroo31@gmail.com>
2024-08-27 00:18:37 +0300

    all data

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -137,7 +137,7 @@ const handleSignUp = async (event) => {
             <form  className='register1' onSubmit={Submit}>
                
             <div className="login_Header">
-        <h2 style={{fontSize:"25px",color:"white", justifyContent:"center"}}> Login</h2> 
+        <h2 className="headerLogin" > Login</h2> 
     
         
         </div>
@@ -180,7 +180,7 @@ const handleSignUp = async (event) => {
                 </div>
                
                 <div className="reset">
-                    <a  onClick={toggleReset} style={{color:"coral"}} target='_blank' >Forget Password/Reset Password</a>
+                    <a  onClick={toggleReset}  target='_blank' >Forget Password/Reset Password</a>
                     
                 </div>
               
@@ -190,7 +190,7 @@ const handleSignUp = async (event) => {
         ): (
           
         <form  className="SignUp_Form" onSubmit={handleSignUp}>
-         <h2 id="id_signUp" style={{fontSize:"21px", color:"white"}}> SignUp Account</h2> 
+         <h2  className="headerLogin1" style={{fontSize:"21px", }}> SignUp </h2> 
                <div className='input_el'>
                <label htmlFor="input-3">Full Name</label>
                 <input className="form-control" aria-label="With textarea" type="text"  className="input_1" autoComplete="on" required onChange={(e)=>setName(e.target.value)}/>
@@ -205,12 +205,12 @@ const handleSignUp = async (event) => {
                 <div style={{color:"red", margin:"4"}}>{signUpMs}</div>
                 <div>
                   <div className="terms" style={{color:"grey"}}>
-                <input id="checkbox" type="checkbox" required/> <a  > <span id="span1" style={{color:"grey"}}>Terms</span> & Conditions</a>
+                <input id="checkbox" type="checkbox" required/> <a  > <span id="span1" style={{color:"white"}}>Terms & Conditions</span> </a>
           </div>
                     <button className="submit2" 
                 type='submit'>Sign Up</button>
                 </div>
-*
+
                 </div>
                 
                 
@@ -223,12 +223,12 @@ const handleSignUp = async (event) => {
                <div className="createAcc">
                 <b className="bottom1"> {!isFormVisible ?(<span>No account?</span>):(<span>Have account ?</span>)}
               <a onClick={toggleForm} className="sign_up">
-              {!isFormVisible ?(<span style={{fontSize:"20px", color:"white"}} id="id_create" >SignUp</span>):(<span id="id_create" >SignIn</span>)} </a></b><br></br>
+              {!isFormVisible ?(<span style={{fontSize:"20px", border:"none"}} id="id_create" >SignUp</span>):(<span id="id_create" >Login</span>)} </a></b><br></br>
                 <span className="bottom2"><span className="bottom4">Manage cookies</span> & <span className="bottom3">opt out</span></span>
                </div> 
 
          <div className="login_footer">
-               <div className="col-1"><b id="b1">@copyRight||ConnectTeam.com</b></div>
+               <div className="col-1"><b id="b1">@copyRight||Zack.com</b></div>
                 <div className="col-2"><b id="b2">
                 <Icon icon="ic:baseline-facebook" />
                 <Icon id="b3" icon="teenyicons:instagram-solid" />
