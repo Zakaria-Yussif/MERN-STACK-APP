666d54975b3b10bcd78a13c5f41a6f9800568e04
Zakaria-Yussif <zackyaroo31@gmail.com>
2024-08-30 00:20:40 +0300

    game over

--- frontend/src/Pages/AI.js
+++ frontend/src/Pages/AI.js
@@ -8,9 +8,16 @@ function AI() {
 
     const [response, setResponse] = useState('');
     const [sendingMsg, setSendingMsg]=useState("")
- 
+  const [userMessage, setUserMessage]=useState([])
 
     const sendMessage = async () => {
+        if(sendingMsg.length ===0){
+            alert("Write Something")
+            return
+        }
+setUserMessage([...userMessage,sendingMsg])
+setSendingMsg("")
+
         try {
             const response = await axios.post("http://localhost:9000/chat", sendingMsg);
             console.log("sending", response);
@@ -27,11 +34,17 @@ function AI() {
             <div className="AI">
         <div className="AI_image"><img id="AI_imgUrl" src="https://cdn-images-1.medium.com/max/1600/1*PKlBOfhHY3AXbsa0kUf9tA.png"/></div>
  <div className="AI_container">
-<div className="colAI AI_message">{response}</div>
-<div className="colAI AI_footer" >
+
+{userMessage.map((item,k)=>(
+    <div className="userMessage">{item}</div>
+
+))}
+
+<div className=" AI_footer" >
+<div className="col text">
 <textarea 
               id="textAreaPhone"
-        className="outline-primary input_AI"
+        className="outline-primary textAreaPhone "
         // value={sendingMsg}
         // ref={inputRef} 
         //  onChange={handleTyping}
@@ -40,10 +53,12 @@ function AI() {
         className="form-control" aria-label="With textarea"
         cols="40"
       > </textarea>
-
-<button onClick={sendMessage} id="AI_ICON" style={{margin:"0px 100px 0px "}} > <Icon  style={{fontSize:"30px", margin:"-50px 0px"}}  icon="zondicons:send" /></button>
+      
+      </div>
+      <div className="col send">
+<button onClick={sendMessage} id="AI_ICON"  > <Icon  style={{fontSize:"30px", margin:"-50px 0px"}}  icon="zondicons:send" /></button>
 </div>
-
+</div>
  </div>
             </div>
         </>
